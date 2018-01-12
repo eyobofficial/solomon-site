d8ed35f4eb9383589541b1506e831e5e7c5cfe60
Eyob Tariku <eyobtariku@gmail.com>
2018-01-12 16:05:29 +0300

    Implement Accordion Tabs to Mobile Service Section

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,11 @@
 $(function() {
-    $( "#service-accordion" ).accordion();
+    $(function(){
+        $('.panel-body').not(':first').addClass('hide');
+        $('.panel-heading').click(function(){
+            $('.panel-body').removeClass('hide').addClass('hide');
+            $('.panel-heading').find('i').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+            $(this).find('i').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+            $(this).next('.panel-body').toggleClass('hide');
+        })
+    });
 } );
