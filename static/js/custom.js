a589602839485b2199c2f3e36bf2e9e1bde8d37b
eyob tariku <eyobtariku@gmail.com>
2018-01-14 16:20:40 +0300

    Create accordion list of all services for mobile
    Use jQuery UI for creating accordion view of all
    service list for mobile views. Change the styling
    to match the site color scheme

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,6 @@
 $(function() {
-    $(function(){
-        $('.panel-body').not(':first').addClass('hide');
-        $('.panel-heading').click(function(){
-            $('.panel-body').removeClass('hide').addClass('hide');
-            $('.panel-heading').find('i').removeClass('fa-chevron-down').addClass('fa-chevron-right');
-            $(this).find('i').removeClass('fa-chevron-right').addClass('fa-chevron-down');
-            $(this).next('.panel-body').toggleClass('hide');
-        })
-    });
-} );
+   $( "#accordion" ).accordion({
+       icons: false,
+       heightStyle: "content",
+});
+});
