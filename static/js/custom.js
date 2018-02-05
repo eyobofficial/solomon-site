924d74200e2c1ae896e68059c44e42456f946aba
eyob tariku <eyobtariku@gmail.com>
2018-02-05 23:46:13 +0300

    Add icons to team staff and service list accordions

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,28 @@
 $(function() {
-   $( ".accordion" ).accordion({
+	// Team Accordion
+	$( "#team-accordion" ).accordion({
+	    icons: false,
+	    heightStyle: "content",
+	});
+
+	$('#team-accordion .ui-accordion-header-active .action-icon').html('<i class="fa fa-minus"></i>');
+
+	$('#team-accordion .ui-accordion-header').click(function(){
+		    $('#team-accordion .ui-accordion-header .action-icon').html('<i class="fa fa-plus"></i>');
+		    $(this).find('.action-icon').html('<i class="fa fa-minus"></i>');
+	});
+
+   // Our Service Accordion 
+   $( "#service-accordion" ).accordion({
        icons: false,
+       collapsible: true,
+       active: false,
        heightStyle: "content",
-});
+   });
+
+   $('#service-accordion .ui-accordion-header').click(function(){
+   	   $('#service-accordion i.fa').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+   	   $('#service-accordion .ui-accordion-header-active i.fa').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+   });
+
 });
